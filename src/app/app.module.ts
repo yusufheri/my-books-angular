3dffcfed1ab68182349ed21a8635f301c8263e98
YUSUF HERI <yusufheri64@gmai.com>
2019-04-13 23:54:57 +0100

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+
+import { AppComponent } from "./app.component";
+import { FormsModule } from "@angular/forms";
+import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { SingleBookComponent } from './book-list/single-book/single-book.component';
+import { BookFormComponent } from './book-list/book-form/book-form.component';
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  declarations: [AppComponent, SignupComponent, SigninComponent, BookListComponent, SingleBookComponent, BookFormComponent, HeaderComponent],
+  imports: [BrowserModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
